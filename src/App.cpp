84ecd3b12553312db95cc1cc082dff556d37cda4
Brian Ok <brianok924@gmail.com>
2022-06-23 16:20:59 -0400

    changes to match changes to core graphics

--- src/App.cpp
+++ src/App.cpp
@@ -4,37 +4,108 @@ using namespace Core;
 class App : public Core::CoreApp
 {
 private:
+    // Everything needed to make a Square on the screen;
     Renderer renderer;
     WindowsPlatform window; 
-    float vertices[24]
-    {
-        // Coords    // texCoords
-         1.0f, -1.0f,  1.0f, 0.0f,
-        -1.0f, -1.0f,  0.0f, 0.0f,
-        -1.0f,  1.0f,  0.0f, 1.0f,
+    VAO vao;
+    VBO vbo;
+    IBO ibo;
+    VBOLayout layout;
+    Shader shader;
+    ImGuiClass imgui;
 
-         1.0f,  1.0f,  1.0f, 1.0f,
-         1.0f, -1.0f,  1.0f, 0.0f,
-        -1.0f,  1.0f,  0.0f, 1.0f
+    float vertices[8] 
+    {
+        // Coords       // texCoords
+        -0.5f, -0.5f, //  0.0f, 0.0f,  // 0
+         0.5f, -0.5f, //  1.0f, 0.0f,  // 1
+         0.5f,  0.5f, //  1.0f, 1.0f,  // 2
+        -0.5f,  0.5f //  0.0f, 1.0f   // 3
     };
+
+    GLuint indices[6]  
+    {
+        0, 1, 2,
+        2, 3, 0
+    };
+
+    // Vertices and Indices Explanition // The Quad is made up of 2 seperate triangles
+    //
+    // -1.0, 1.0 // 3                    1.0, 1.0 // 2
+    //  ---------------------------------
+    //  |                           /   |
+    //  |                         /     |
+    //  |                       /       |
+    //  |                    /          | 
+    //  |                  /            |
+    //  |               /               |
+    //  |            /                  |
+    //  |         /                     |
+    //  |      /                        | 
+    //  |   /                           |
+    //  | /                             |
+    //  ---------------------------------
+    // -1.0, -1.0 // 0                   1.0, -1.0 // 1
 public:
     App()
-        : CoreApp(), window(WindowsPlatform()), renderer(Renderer())
+        : CoreApp(), window(WindowsPlatform("Square - CoreGraphics", 800, 800))
     {
+        if(!LoadOpenGLContext())
+        {
+            window.Close();
+        }
         window.setVSync(true);
 
-        
+
+        vao.init();
+        vbo.init(vertices, sizeof(vertices), GL_STATIC_DRAW);
+        ibo.init(indices, sizeof(indices));
+        shader.init("../../res/Color.vert", "../../res/Color.frag");
+        imgui.init(window.getWindow());
+
+
+        layout.Push<float>(2); // Our data contains 2 floats of x, y so we push to the layout that we have 2 floats so it reads the data correctly
+        vao.AddBuffer(vbo, layout);
+        shader.Bind();
+
+
+        vao.Unbind();
+        vbo.Unbind();
+        ibo.Unbind();
+        shader.Unbind();
+
     }
 
-    ~App() 
+    ~App()
     {
+        vao.Unbind();
+        vbo.Unbind();
+        ibo.Unbind();
+        shader.Unbind();
     }
 
     void run()
     {
+        float ClearColor[] = { 1.0f, 1.0f, 1.0f, 1.0f};
         while(window.WindowRunning)
         {
+            renderer.Clear();
+            glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+            
+            shader.Bind();
+            shader.SetUniform4f("aColor", ClearColor[0], ClearColor[1], ClearColor[2], ClearColor[3]);
+
+            renderer.Draw(vao, ibo, shader);
+
+            imgui.ImGuiNewFrame();
+            ImGui::Begin("Options");
+            ImGui::ColorEdit4("Clear Color", ClearColor);
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            ImGui::End();
+            imgui.ImGuiRenderFrame();
+
             window.onUpdate();
+
         }
     }
 };
